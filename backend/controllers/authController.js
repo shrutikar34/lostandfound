8029836b8725523a502ceac09e8644aec66814b5
POBOROJO <parijatbhatt1@gmail.com>
2024-08-17 16:35:36 +0530

    Changes in the authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,96 +1,113 @@
-import {User,UserSchemaZod} from "../models/User.js";
+import { User, UserSchemaZod } from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config({
-    path:"../controller/.env"
+  path: "../controller/.env",
 });
 
-export const Register = async (req,res)=>{
-    try{
-        const {name,email,password,phone} = req.body;
-        if(!name || !email || !password || !phone){
-            return res.status(401).json({
-                message:"All fiels are required",
-                success:false
-            })
-        }
-        const user = await User.findOne({email});
-        if(user){
-            return res.status(401).json({
-                message:"User already exists",
-                success:false
-            })
-        }
-        
-        const hashedPassword = await bcrypt.hash(password,10)
-        const hashedPhone = await bcrypt.hash(phone,10) 
+export const Register = async (req, res) => {
+  try {
+    const validatedData = UserSchemaZod.parse(req.body); // here we are validating the data according to the zod schema
+    const { name, email, password, phone } = validatedData;
 
-        await User.create({
-            name,
-            email,
-            password:hashedPassword,
-            phone:hashedPhone
-        })
-        return res.status(201).json({
-            message:"Account created successfully",
-            success:true
-        })
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+        success: false,
+      });
     }
-    catch(error){
-        return res.status(500).json({
-            message: "Internal server error",
-            success: false
-        });
-    }
-}
 
-export const Login = async (req,res)=>{
-    try{
-        const {email,password} = req.body;
-        if(!email || !password){
-            return res.status(401).json({
-                message:"All fiels are required",
-                success:false
-            })
-        }
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(401).jsom({
-                message:"User not found",
-                success:false
-            })
-        }
-        const isMatch = await bcrypt.compare(password,user.password);
-        if(!isMatch){
-            return res.status(401).json({
-                message:"Incorrect password or email",
-                success:false
-            })
-        }
-        const tokenData ={
-            userId:user._id,
-        }
-        const token = jwt.sign(tokenData,process.env.JWT_SECRET,{
-            expiresIn:"1d"
-        });
-        
-        return res.status(201).cookie("token",token,{expiresIn:"1d",httpOnly:true}).json({
-            message:`Welcome back ${user.name}`,
-            success:true
-        })
-    }
-    catch(error){
-        return res.status(500).json({
-            message: "Internal server error",
-            success: false
-        });
-    }
-}
+    //Here we are hashing the password and phone number before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedphone = await bcrypt.hash(phone, 10);
 
-export const Logout = (req,res)=>{
-    return res.cookie("token","", {expiresIn:new Date(Date.now())}).json({
-        message:"Logout success",
-        success:true
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      phone: hashedphone,
+    });
+    return res.status(201).json({
+      message: "User created successfully",
+      success: true,
+    });
+  } catch (error) {
+    if (error.errors) {
+      return res.status(400).json({
+        message: error.errors[0].message,
+        success: false,
+      });
+    }
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const validatedData = UserSchemaZod.pick({
+      email: true,
+      password: true,
+    }).parse(req.body);
+
+    const { email, password } = validatedData;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).jsom({
+        message: "User not found",
+        success: false,
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Incorrect password or email",
+        success: false,
+      });
+    }
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    return res
+      .status(201)
+      .cookie("token", token, { expiresIn: "1d", httpOnly: true })
+      .json({
+        message: `Welcome back ${user.name}`,
+        success: true,
+      });
+  } catch (error) {
+    if (error.errors) {
+      return res.status(400).json({
+        message: error.errors[0].message,
+        success: false,
+      });
+    }
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+export const Logout = (req, res) => {
+  return res
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+      sameSite: "strict",
     })
-}
+    .status(200)
+    .json({
+      message: "Logout successful",
+      success: true,
+    });
+};
