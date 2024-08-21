caf29b3d0ac1f6991804388bcb1105104bb2e046
POBOROJO <parijatbhatt1@gmail.com>
2024-08-21 16:52:04 +0530

    Added: userController.js getProfile, updatedProfile, deleteProfile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,3 +26,62 @@ export const getMyProfile = async (req, res) => {
         });
     }
 }
+
+export const updateMyProfile = async (req, res) => {
+    try{
+        const validatedData = UserSchemaZod.partial().parse(req.body);
+        
+        const {name, email, password, phone} = validatedData;
+
+        const updatedData = {}; //here we are creating an object to store the updated data
+
+        // here we are checking if the data is not empty and if it is not empty then we are updating the data
+        if(name) updatedData.name = name;
+        if(email) updatedData.email = email;
+        if(password) updatedData.password = await bcrypt.hash(password, 10);
+        if(phone) updatedData.phone = await bcrypt.hash(phone, 10);
+
+        const updatedProfile = await User.findByIdAndUpdate(req.user._id, updatedData, {new:true, select:"-password -phone"});
+
+        if(!updatedProfile){
+            return res.status(404).json({
+                message:"User not found",
+                success:false,
+            })
+        }
+
+        return res.status(200).json({
+            message:"Profile updated successfully",
+            success:true,
+            user:updatedProfile
+        })
+    }
+    catch(error){
+        console.error("Error in updateMyProfile:", error);
+        return res.status(400).json({
+            message: error.errors ? error.errors[0].message : "Invalid input data",
+            success: false,
+        });
+    }
+}
+
+export const deleteMyProfile = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const deleteUser = await User.findByIdAndDelete(id);
+
+        if(!deleteUser){
+            return res.status(404).json({
+                message:"User not found",
+                success:false,
+            })
+        }
+    }
+    catch(error){
+        console.error("Error in deleteMyProfile:", error);
+        return res.status(400).json({
+            message: error.errors ? error.errors[0].message : "Invalid input data",
+            success: false,
+        });
+    }
+}
